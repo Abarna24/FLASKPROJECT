5b81729a620abc367ff8118729fc2f3775e19ca3
Abarna24 <abarnanatarajan24@gmail.com>
2021-04-19 14:47:30 +0530

    validation added

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import os
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from werkzeug.security import generate_password_hash, check_password_hash
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Length
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
 database_file = "sqlite:///{}".format(os.path.join(project_dir, "mydb.db"))
-print("print:",database_file)
+print("print:", database_file)
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 
 db = SQLAlchemy(app)
 
 
+# validation
+class Form(FlaskForm):
+    name = StringField('name', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=6, max=80)])
+    remember = BooleanField('remember me')
+
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    if request.form == 'POST':
+    form = Form()
+    if form.validate_on_submit():
         name = Detail(name=request.form.get("name"))
         phone = Detail(phone=request.form.get("phone"))
         email = Detail(email=request.form.get("email"))
